6a58b4c30abceccd5d173b85a5bdcab2bffd6616
Justin Jackson <justin@transistor.fm>
2021-02-10 16:11:38 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [
+    "./index.html"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
